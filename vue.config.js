67a967f5c1ff874b96e84135da1550620531392f
Lv Lei, CIS-21 <lei.lv@lingyue-digital.com>
2022-03-25 15:13:23 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -15,7 +15,7 @@ const name = defaultSettings.title || 'vue Admin Template'; // page title
 // You can change the port by the following methods:
 // port = 9528 npm run dev OR npm run dev --port = 9528
 const port = process.env.port || process.env.npm_config_port || 9528; // dev port
-const proxyHost = process.env.VUE_APP_PROXY_TARGET || 'https://dealer-dev.bmw-emall.cn/api/'
+const proxyHost = process.env.VUE_APP_PROXY_TARGET || 'https://dealer-dev.xxxxxxxxxx/api/'
 // console.log('///////////////////////////>>>>>>>>', process.env.NODE_ENV)
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
